17455c724878c40615aec8e4b7199b5e6e0cf29c
NateM <nmendoza12@mail.pima.edu>
2019-02-13 14:39:06 -0700

    Added a player object
    plaer has balll and position

---
+++ Chip and Putt/Assets/MyScripts/DirectionSlider.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DirectionSlider : MonoBehaviour
+{
+
+    Slider directionSlider;
+    bool hitRight = false;
+    bool hitLeft = true;
+
+
+    void Start()
+    {
+        directionSlider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ActivateDirectionSlider();
+    }
+
+    public void ActivateDirectionSlider()
+    {
+
+        if (hitLeft == true)
+        {
+            directionSlider.value += Time.deltaTime;
+            if (directionSlider.value >= directionSlider.maxValue - 1)
+            {
+                hitRight = true;
+                directionSlider.value -= Time.deltaTime;
+            }
+        }
+        if (hitRight == true)
+
+        {
+            directionSlider.value -= Time.deltaTime;
+            if (directionSlider.value <= directionSlider.minValue + 1)
+            {
+                hitLeft = true;
+                directionSlider.value += Time.deltaTime;
+            }
+
+
+        }
+    }
+}
